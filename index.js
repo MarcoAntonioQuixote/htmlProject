c494f2acee83f6651e2f4fdd9eb042e05fbc6f33
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2023-09-20 21:08:13 -0600

    Our first HTML/CSS/BS project

---
+++ index.js
@@ -0,0 +1,10 @@
+let value = document.getElementById()
+
+fetch('https://pokeapi.co/api/v2/pokemon/mew')
+    .then(res => res.json())
+    .then(data => {
+        let img = data.sprites.front_default;
+        let picture = document.createElement('img');
+        picture.src = img;
+        document.body.append(picture);
+    });
